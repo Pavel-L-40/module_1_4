5369ba21c5f85596d612696d69ca04424f030937
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-08-31 22:34:51 +0300

    "Организация программ и методы строк"

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = 'Пример результата выполнения программы'
+my_string = input('Введите строку для примера: ')
+print((len(my_string)))
+print((my_string.upper()))
+print((my_string.lower()))
+print((my_string.replace(' ', '')))
+print(my_string[0], '\n'+ my_string[-1])
